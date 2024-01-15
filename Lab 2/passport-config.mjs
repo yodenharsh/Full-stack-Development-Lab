fbbf75aa8beb6d8705a2e57c8d0e4a7015e1072c
Harsh Morayya <harshmorayya@ymail.com>
2024-01-15 17:58:55 +0530

    Added Lab 2

---
+++ Lab 2/passport-config.mjs
@@ -0,0 +1,27 @@
+import { Strategy } from "passport-local";
+import bcrypt from "bcrypt";
+
+function initialize(passport, getUserByEmail, getUserById) {
+  const authenticateUser = async (email, password, done) => {
+    const user = getUserByEmail(email);
+    if (user == null) {
+      return done(null, false, { message: "No user with that email" });
+    }
+
+    try {
+      if (await bcrypt.compare(password, user.password)) {
+        done(null, user);
+      } else {
+        return done(null, false, { message: "Invalid credentials" });
+      }
+    } catch (e) {
+      return done(e);
+    }
+  };
+
+  passport.use(new Strategy({ usernameField: "email" }, authenticateUser));
+  passport.serializeUser((user, done) => done(null, user.id));
+  passport.deserializeUser((id, done) => done(null, getUserById(id)));
+}
+
+export default initialize;
---
+++ Lab 2/server.mjs
@@ -0,0 +1,92 @@
+import express from "express";
+import bcrypt from "bcrypt";
+import initialize from "./passport-config.mjs";
+import flash from "express-flash";
+import passport from "passport";
+import session from "express-session";
+import methodOverride from "method-override";
+
+const app = express();
+app.use(express.json());
+app.set("view-engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
+app.use(flash());
+app.use(
+  session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app
+  .use(passport.initialize())
+  .use(passport.session())
+  .use(methodOverride("_method"));
+
+initialize(
+  passport,
+  (email) => users.find((user) => user.email === email),
+  (id) => users.find((user) => user.id === id)
+);
+
+const users = [];
+
+app.get("/", checkAuthenticated, (req, res) => {
+  res.render("index.ejs", { name: req.user.name });
+});
+
+app.get("/login", checkNotAuthenticated, (req, res) => {
+  res.render("login.ejs");
+});
+
+app.post(
+  "/login",
+  checkNotAuthenticated,
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+    failureFlash: true,
+  })
+);
+
+app.get("/register", checkNotAuthenticated, (req, res) => {
+  res.render("register.ejs");
+});
+
+app.post("/register", checkNotAuthenticated, async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    users.push({
+      id: Date.now().toString(),
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+    res.redirect("/login");
+  } catch {
+    res.redirect("/register");
+  }
+});
+
+app.delete("/logout", (req, res) => {
+  req.logOut();
+  res.redirect("/login");
+});
+
+function checkAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+
+  res.redirect("/login");
+}
+
+function checkNotAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return res.redirect("/");
+  }
+  next();
+}
+
+app.listen(3000);
